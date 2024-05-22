0a8cca51ad0ec7775cbb5531364e3c442362b284
天空Blond <hurui200320@skyblond.info>
2024-05-22 19:52:37 +0800

    don't source cuda when in aws

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,11 @@ repositories {
 
 dependencies {
     implementation("org.bytedeco:javacv-platform:1.5.10")
-    implementation("org.bytedeco:cuda-platform:12.3-8.9-1.5.100")
-    implementation("org.bytedeco:cuda-platform-redist:12.3-8.9-1.5.10")
+    if (System.getenv("AWS_EXECUTION_ENV") == null) {
+        // not in AWS
+        implementation("org.bytedeco:cuda-platform:12.3-8.9-1.5.100")
+        implementation("org.bytedeco:cuda-platform-redist:12.3-8.9-1.5.10")
+    }
 
     implementation("com.github.ajalt.colormath:colormath:3.5.0")
     implementation("com.microsoft.onnxruntime:onnxruntime_gpu:1.17.3")
